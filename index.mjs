3a13b30b465b1a30c6021cab8e508fea39a0011c
Calaway <calaway@users.noreply.github.com>
2023-07-11 00:49:44 -0600

    Guess five random words

--- index.mjs
+++ index.mjs
@@ -1,8 +1,9 @@
 import puppeteer from 'puppeteer';
 // const puppeteer = await import('puppeteer')
 
-import actions from './src/actions.mjs'
+import actions from './src/actions.mjs';
 import { sleep } from './src/utils.mjs';
+import { wordList } from './src/word-list-small.mjs';
 
 (async () => {
   const browser = await puppeteer.launch({headless: false}); // headless values: false or 'new'
@@ -13,14 +14,30 @@ import { sleep } from './src/utils.mjs';
   await page.locator('button[data-testid="Play"]').click();
   await page.locator('button[aria-label="Close"]').click();
 
-  await actions.submitWord(page, 'crane');
-  await actions.submitWord(page, 'lousy');
-  await actions.submitWord(page, 'fight');
-  await actions.submitWord(page, 'break');
-  await actions.submitWord(page, 'kazoo');
-  await actions.submitWord(page, 'dream');
+  function randomWord(wordList) {
+    return wordList[Math.floor(Math.random() * wordList.length)];
+  }
 
-  await sleep(10000);
+  let word = randomWord(wordList);
+  console.log(word);
+  await actions.submitWord(page, word);
+  word = randomWord(wordList);
+  console.log(word);
+  await actions.submitWord(page, word);
+  word = randomWord(wordList);
+  console.log(word);
+  await actions.submitWord(page, word);
+  word = randomWord(wordList);
+  console.log(word);
+  await actions.submitWord(page, word);
+  word = randomWord(wordList);
+  console.log(word);
+  await actions.submitWord(page, word);
+  word = randomWord(wordList);
+  console.log(word);
+  await actions.submitWord(page, word);
+
+  await sleep(10_000);
 
   await browser.close();
 })();
