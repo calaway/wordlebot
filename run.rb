fd73b5e30784ff656e2fd4cadb2a169a9fd1c5c8
Calaway <calaway@users.noreply.github.com>
2023-09-04 14:56:26 -0600

    Create board class

--- run.rb
+++ run.rb
@@ -1,37 +1,16 @@
-require 'selenium-webdriver'
-require 'capybara/dsl'
 require 'pry'
 
-Capybara.default_driver = :selenium
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
+require_relative 'src/board'
 
-include Capybara::DSL
+board = Board.new
 
-# Navigate to game
-visit('https://www.nytimes.com/games/wordle/index.html')
-click_button('Continue')
-click_button('Play')
-find('button[aria-label="Close"]').click
+word_list = board.load
 
-# Submit first word
-def submit_word(row, word)
-  letters = word.split('')
-  while find("div[aria-label='Row #{row}'] div[aria-label*='1st letter']")['data-state'] == 'empty' do
-    find("div[aria-label='Keyboard'] button[data-key='#{letters.first}']").click
-  end
-  letters[1..].each do |letter|
-    find("div[aria-label='Keyboard'] button[data-key='#{letter}']").click
-  end
-  find("div[aria-label='Keyboard'] button[data-key='↵']").click
-end
-
-submit_word(1, 'brave')
-submit_word(2, 'lousy')
-submit_word(3, 'shake')
-submit_word(4, 'gravy')
-submit_word(5, 'large')
-submit_word(6, 'troop')
+board.submit_word(1, word_list.sample)
+board.submit_word(2, word_list.sample)
+board.submit_word(3, word_list.sample)
+board.submit_word(4, word_list.sample)
+board.submit_word(5, word_list.sample)
+board.submit_word(6, word_list.sample)
 
 binding.pry
---
+++ src/board.rb
@@ -0,0 +1,38 @@
+require 'selenium-webdriver'
+require 'capybara/dsl'
+
+Capybara.default_driver = :selenium
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+class Board
+  include Capybara::DSL
+
+  def initialize
+  end
+
+  def load
+    visit('https://www.nytimes.com/games/wordle/index.html')
+
+    word_list = File.readlines('./src/word-list-small.txt').map(&:chomp)
+    puts "Word list loaded. #{word_list.length} words found."
+
+    click_button('Continue')
+    click_button('Play')
+    find('button[aria-label="Close"]').click
+
+    word_list
+  end
+
+  def submit_word(row, word)
+    letters = word.split('')
+    while find("div[aria-label='Row #{row}'] div[aria-label*='1st letter']")['data-state'] == 'empty' do
+      find("div[aria-label='Keyboard'] button[data-key='#{letters.first}']").click
+    end
+    letters[1..].each do |letter|
+      find("div[aria-label='Keyboard'] button[data-key='#{letter}']").click
+    end
+    find("div[aria-label='Keyboard'] button[data-key='↵']").click
+  end
+end
